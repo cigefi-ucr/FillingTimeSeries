0cb8314090a9407d7376bfae82463c51f451356e
rolandojduartem <rolando.duartemejias@gmail.com>
2021-09-09 18:59:00 -0600

    Updating setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,29 @@
 from distutils.core import setup
 setup(
-  name = 'FillingTimeSeries',         # How you named your package folder (MyLib)
-  packages = ['FillingTimeSeries'],   # Chose the same as "name"
-  version = '0.8',      # Start with a small number and increase it with every change you make
-  license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
-  description = 'Filling Time series: Package to fill missing values in geophysical time series in Python',   # Give a short description about your library
-  author = 'Rolando Jesus Duarte Mejias & Erick Rivera Fernandez',                   # Type in your name
-  author_email = 'rolando.duartemejias@gmail.com',      # Type in your E-Mail
-  url = 'https://github.com/user/FillingTimeSeries',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
+  name = 'FillingTimeSeries',    
+  packages = ['FillingTimeSeries'], 
+  version = '0.8.0',   
+  license='MIT', #License from here: https://help.github.com/articles/licensing-a-repository
+  description = 'Filling Time series: Package to fill missing values in geophysical time series in Python',   #Short description
+  author = 'Rolando Jesus Duarte Mejias & Erick Rivera Fernandez',    
+  author_email = 'rolando.duartemejias@gmail.com',
+  url = 'https://github.com/rolandojduartem/FillingTimeSeries', #github or website
+  download_url = 'https://github.com/rolandojduartem/FillingTimeSeries/archive/refs/tags/v_0_8_0.tar.gz',    #Download file
+  keywords = ['Time Series', 'Missing values', 'Metereology', "Geophysics", "Metereological"],
+  install_requires=[       # I get to this in a second
+          'statsmodels',
+          'scikit-learn',
+          'matplotlib',
       ],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
-    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Development Status :: 4 - Beta', # "3 - Alpha", "4 - Beta" or "5 - Production/Stable"
+    'Intended Audience :: Developers', # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
-    'Programming Language :: Python :: 3.4',
-    'Programming Language :: Python :: 3.5',
+    'License :: OSI Approved :: MIT License', #license
+    'Programming Language :: Python :: 3', #Pyhton versions
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
   ],
 )
